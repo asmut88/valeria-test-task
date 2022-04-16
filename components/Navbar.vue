b84f1406e8865de6f2894531475d6e7b3b186e2f
asmut88 <asmut88@gmail.com>
2022-04-16 11:55:32 +0600

    Navbar finished

---
+++ components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Nuxt SSR</a>
+
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <nuxt-link to="/" active-class="active" class="nav-link"  no-prefetch exact>
+              Home
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/about" active-class="active" class="nav-link" >
+              About
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h1>About page</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa facere
+      impedit, velit consectetur laboriosam sequi unde, magnam cum ex
+      reprehenderit deserunt non, voluptatibus tenetur odit. Commodi earum, a
+      amet quos pariatur ratione!
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi
+      corrupti culpa, possimus nemo nihil?
+    </p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p>Lorem ipsum dolor sit amet.</p>
+  </section>
+</template>
