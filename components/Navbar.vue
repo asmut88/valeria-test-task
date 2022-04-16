729435c6c295a8bced8f115d3fc5da39f520e8de
asmut88 <asmut88@gmail.com>
2022-04-16 14:16:32 +0600

    Request Axios users data

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,18 +1,34 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Nuxt SSR</a>
+      <a class="navbar-brand" href="#">Valeria TT</a>
 
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <nuxt-link to="/" active-class="active" class="nav-link"  no-prefetch exact>
+            <nuxt-link
+              to="/"
+              active-class="active"
+              class="nav-link"
+              no-prefetch
+              exact
+            >
               Home
             </nuxt-link>
           </li>
           <li class="nav-item">
-            <nuxt-link to="/about" active-class="active" class="nav-link" >
-              About
+            <nuxt-link to="/users" active-class="active" class="nav-link">
+              Users
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/about" active-class="active" class="nav-link">
+              FAQ
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/login" active-class="active" class="nav-link">
+              Login
             </nuxt-link>
           </li>
         </ul>
