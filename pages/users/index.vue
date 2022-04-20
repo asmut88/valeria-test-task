135c6bf7d9f1fba479320fc85e7b372402da3ab5
asmut88 <asmut88@gmail.com>
2022-04-20 13:40:31 +0600

    filter + pagination

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,13 +1,23 @@
 <template>
-  <section>
-    <h1>{{ pageTitle }}</h1>
+  <table>
+    <thead>
+      <tr>
+        <th @click="sort('id')">id</th>
+        <th @click="sort('name')">Name</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in sortedUsers" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td><a href="#" @click.prevent="openUser(user)"> {{ user.name }} </a></td>
+      </tr>
+    </tbody>
 
-    <ul>
-      <li v-for="user of users" :key="user.id">
-        <a href="#" @click.prevent="openUser(user)"> {{ user.name }} </a>
-      </li>
-    </ul>
-  </section>
+    <p>
+      <button @click="prevPage">Previous</button>
+      <button @click="nextPage">Next</button>
+    </p>
+  </table>
 </template>
 
 <script>
@@ -23,15 +33,57 @@ export default {
   },
   data: () => ({
     pageTitle: "Users page",
+    users: [],
+    currentSort: "name",
+    currentSortDir: "asc",
+
+    //**Pagination */
+    pageSize: 3,
+    currentPage: 1,
   }),
+  created: function () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((res) => {
+        this.users = res;
+      });
+  },
   computed: {
-    users() {
-      return this.$store.getters["users/users"];
+    /*users() {
+      return this.$store.getters["users/users"]; //**Попытка в vuex 
+    },*/
+    sortedUsers: function () {
+      return this.users.sort((a,b) => {
+		let modifier = 1;
+		if(this.currentSortDir === 'desc') modifier = -1;
+		if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+		if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+		return 0;
+	}).filter((row, index) => {
+		let start = (this.currentPage-1)*this.pageSize;
+		let end = this.currentPage*this.pageSize;
+		if(index >= start && index < end) return true;
+	});
     },
   },
   methods: {
     openUser(user) {
       this.$router.push("/users/" + user.id);
+    },
+    sort: function (s) {
+      //if s == current sort, reverse
+      if (s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
+      }
+      this.currentSort = s;
+    },
+    //**Pagination */
+    nextPage: function () {
+      if (this.currentPage * this.pageSize < this.users.length)
+        this.currentPage++;
+    },
+    prevPage: function () {
+      if (this.currentPage > 1) this.currentPage--;
     },
   },
 };
